2b3fdb94bc89afd9c1dd4bb392dc72591ac3c310
anilachundi <anilachundi@gmail.com>
2022-12-03 15:58:15 -0600

    input timeIndoors, productiveHours, physicalActivityHours

--- app/src/main/java/com/example/cs124h/MainActivity.java
+++ app/src/main/java/com/example/cs124h/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cs124h;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,13 +14,20 @@ public class MainActivity extends AppCompatActivity {
     private String name;
     private int age;
     private String gender;
+    private int timeIndoors;
+    private int productiveHours;
+    private int physicalActivityHours;
 
     private EditText nameInput;
     private EditText ageInput;
     private EditText genderInput;
+    private EditText timeIndoorsInput;
+    private EditText productiveHoursInput;
+    private EditText physicalActivityHoursInput;
 
     private Button submitButton;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         nameInput = (EditText) findViewById(R.id.nameInput);
         ageInput = (EditText) findViewById(R.id.ageInput);
         genderInput = (EditText) findViewById(R.id.genderInput);
+        timeIndoorsInput = (EditText) findViewById(R.id.timeIndoorsInput);
+        productiveHoursInput = (EditText) findViewById(R.id.productiveHoursInput);
+        physicalActivityHoursInput = (EditText) findViewById(R.id.physicalActivityHoursInput);
 
         submitButton = (Button) findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
                 name = nameInput.getText().toString();
                 age = Integer.valueOf(ageInput.getText().toString());
                 gender = genderInput.getText().toString();
+                timeIndoors = Integer.valueOf(timeIndoorsInput.getText().toString());
+                productiveHours = Integer.valueOf(productiveHoursInput.getText().toString());
+                physicalActivityHours = Integer.valueOf(physicalActivityHoursInput.getText().toString());
             }
         });
     }
